7af3eb706f660579b2c4ecb0cd572ace6505b26f
akrom17ua <akromurnashev17@gmail.com>
2024-06-17 13:10:18 +0500

    ch

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -7,3 +7,8 @@ from .serializers import MenuItemSerializer
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
