05132d6f239fd33149b64738eeb2f7f3bfd0920b
akrom17ua <akromurnashev17@gmail.com>
2024-06-17 16:32:01 +0500

    ch

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,19 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
-@api_view()
+
+@api_view(['GET', 'POST'])
 def menuitems(request):
-    items = MenuItem.objects.select_related('category').all()
-    serialized_data = MenuItemSerializer(items, many=True)
-    return Response(serialized_data.data)
+    if request.method == 'GET':
+        items = MenuItem.objects.select_related('category').all()
+        serialized_item = MenuItemSerializer(items, many=True)
+        return Response(serialized_item.data)
+    if request.method == 'POST':
+        serialized_item = MenuItemSerializer(data=request.data)
+        serialized_item.is_valid(raise_exception=True)
+        serialized_item.save()
+        return Response(serialized_item.data, status.HTTP_201_CREATED)
 
 @api_view()
 def menuitem(request, id):
-    item = MenuItem.objects.get(pk=id)
+    item = get_object_or_404(MenuItem,pk=id)
     serialized_data = MenuItemSerializer(item)
     return Response(serialized_data.data)
